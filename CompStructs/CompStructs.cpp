9833919379e3a2596375ecdb5bcc0ee4f19f14a1
Kayden Miller <KaydenMiller1@gmail.com>
2017-09-06 11:09:55 -0600

    Clean up the code for Sets

--- CompStructs/CompStructs.cpp
+++ CompStructs/CompStructs.cpp
@@ -11,51 +11,6 @@ using std::endl;
 using std::list;
 using CompStructs::Set;
 
-void SubtractSets(list<int> A, list<int> B)
-{
-	list<int> temp = A;
-
-	for (int numA : temp)
-		for (int numB : B)
-			temp.remove(numB);
-
-	cout << "Subtract: ";
-	for (int num : temp)
-		cout << num << " ";
-	cout << endl << "---------------------------" << endl;
-}
-
-void UnionOfSets(list<int> A, list<int> B)
-{
-	list<int> temp = A;		// set temp equal to A
-	temp.merge(B);			// merge B into the temp
-	temp.unique();			// Remove all duplicate values
-
-	cout << "Union: ";
-	for (int num : temp)
-		cout << num << " ";
-	cout << endl << "---------------------------" << endl;
-}
-
-void IntersectionOfSets(list<int> A, list<int> B)
-{
-	list<int> temp;
-
-	for (list<int>::iterator itA = A.begin(); itA != A.end(); itA++)
-	{
-		for (list<int>::iterator itB = B.begin(); itB != B.end(); itB++)
-		{
-			if (*itA == *itB)
-				temp.push_back(*itA);
-		}
-	}
-
-	cout << "Intersection: ";
-	for (int num : temp)
-		cout << num << " ";
-	cout << endl << "---------------------------" << endl;
-}
-
 int main()
 {
 	list<int> setA = { 1, 3, 5, 6, 8 };
@@ -73,10 +28,6 @@ int main()
 	cout << endl << "---------------------------" << endl;
 	cout << "Intersection: " << Set::Intersection(A, B);
 	cout << endl << "---------------------------" << endl;
-
-	//SubtractSets(setA, setB);
-	//UnionOfSets(setA, setB);
-	//IntersectionOfSets(setA, setB);
 
 	int input;
 	std::cin >> input;
